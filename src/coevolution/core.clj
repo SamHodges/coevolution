f47b2396bf85eab48e9ff6c4ff45fcdeeff563ec
Sam <samhodges121@gmail.com>
2022-04-15 23:54:38 -0400

    extremely rough plan

--- src/coevolution/core.clj
+++ src/coevolution/core.clj
@@ -1,6 +1,24 @@
 (ns coevolution.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; TEACHER
+; ingredients- total test set, eg regression input/outputs
+
+; mutation function- change which test sets are being chosen
+
+; error function- look at how much students improved
+
+; main loop for evolving- run every x loops of student evolving
+
+
+
+; STUDENT
+; ingredients- potentially ingredients to find linear regression?
+
+; mutation function- change equation
+
+; error function- how far off given teacher input/outputs answer key
+
+; main loop for evolving:
+; - different students given different test cases
+; - every x loops call teacher so it can evolve
+
